854111c05f6ef91055c7f112420cc397fe03c4a2
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-26 17:21:38 +0300

    db seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            AmbassadorSeeder::class
+            AmbassadorSeeder::class,
+            ProductSeeder::class
         ]);
     }
 }
