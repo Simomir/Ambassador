77bd6b34390d744ac285e64ad1abc9d01ee20376
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-26 18:01:17 +0300

    ide helper generate

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -5,6 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Link
+ *
+ * @property int $id
+ * @property string $code
+ * @property int $user_id
+ * @method static \Illuminate\Database\Eloquent\Builder|Link newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Link whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Link extends Model
 {
     use HasFactory;
