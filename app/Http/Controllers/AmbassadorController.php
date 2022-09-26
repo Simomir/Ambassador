43a5ad73186854a067a8748174a5e32d4627dd27
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-26 15:18:57 +0300

    method logic and api route for ambassadors

--- app/Http/Controllers/AmbassadorController.php
+++ app/Http/Controllers/AmbassadorController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AmbassadorController extends Controller
 {
     public function index()
     {
-
+        return User::where('is_admin', 0)->get();
     }
 }
