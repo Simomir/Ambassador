e7b840895fe1dad5340cde948bf4261cbe364c9c
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-27 12:08:52 +0300

    orders table fields

--- database/migrations/2022_09_27_081132_create_orders_table.php
+++ database/migrations/2022_09_27_081132_create_orders_table.php
@@ -15,6 +15,18 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('transaction_id')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code');
+            $table->string('ambassador_email');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->string('zip')->nullable();
+            $table->tinyInteger('complete')->default(0);
             $table->timestamps();
         });
     }
