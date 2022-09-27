c46681937e0f02adc7ef0ea614015979ee7a9c6d
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-27 09:55:58 +0300

    Link controller method and api route

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
 use Illuminate\Http\Request;
 
 class LinkController extends Controller
 {
-    //
+    public function index($id)
+    {
+        return Link::where('user_id', $id)->get();
+    }
 }
