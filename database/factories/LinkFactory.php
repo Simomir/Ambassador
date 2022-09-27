8fafcb03ffbc7c77a5466978c9ad74df602800b5
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-27 10:03:23 +0300

    Link factory logic

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LinkFactory extends Factory
@@ -14,7 +15,8 @@ class LinkFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'code' => $this->faker->slug,
+            'user_id' => User::inRandomOrder()->first()->id
         ];
     }
 }
