49f6457be2dfb3ec07865d9cbd6dd9e7380f3ca9
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-29 11:37:20 +0300

    user resource logic

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -9,11 +9,15 @@ class UserResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+        ];
     }
 }
