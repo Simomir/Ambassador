47b6e922acd96c0cf00dedc93012a9861a1f9303
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-29 12:24:49 +0300

    2 new api routes

--- routes/api.php
+++ routes/api.php
@@ -52,4 +52,6 @@ Route::prefix('/admin')->group(function () {
 // Ambassador
 Route::prefix('/ambassador')->group(function () {
     common('scope.ambassador');
+    Route::get('/products/frontend', [ProductController::class, 'frontend']);
+    Route::get('/products/backend', [ProductController::class, 'backend']);
 });
