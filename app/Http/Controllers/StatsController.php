8c59bd83538e0c9cacda88770ce0b69442f63346
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-29 18:01:40 +0300

    rafctor to get loged in user for stats controller

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -4,15 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Link;
 use App\Models\Order;
+use Auth;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $user = $request->user();
-        $links = Link::where('user_id', $user->id)->get();
-
+        $links = Link::where('user_id', Auth::user()->id)->get();
         return $links->map(
             function (Link $link) {
                 $orders = Order::where('code', $link->code)->where('complete', 1)->get();
