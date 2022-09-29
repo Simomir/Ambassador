24f1f0da4beeac197c6ea71ae211d3ee25f18995
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-29 16:43:02 +0300

    index function in stats controller

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Link;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class StatsController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $user = $request->user();
+        $links = Link::where('user_id', $user->id)->get();
+
+        return $links->map(
+            function (Link $link) {
+                $orders = Order::where('code', $link->code)->where('complete', 1)->get();
+                return [
+                    'code' => $link->code,
+                    'count' => $orders->count(),
+                    'revenue' => $orders->sum(fn(Order $order) => $order->ambassador_revenue),
+                ];
+            }
+        );
+    }
 }
