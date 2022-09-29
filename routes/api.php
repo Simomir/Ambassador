31bfa46e67150cef0087ce1df620a27f11230104
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-29 10:19:53 +0300

    new routes - ambassador

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,8 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-// Admin
 
+// Admin
 Route::prefix('/admin')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
@@ -40,5 +40,18 @@ Route::prefix('/admin')->group(function () {
         Route::get('/orders', [OrderController::class, 'index']);
 
         Route::apiResource('products', ProductController::class);
+    });
+});
+
+// Ambassador
+Route::prefix('/ambassador')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware(['auth:sanctum', 'scope.admin'])->group(function () {
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::put('/users/info', [AuthController::class, 'updateInfo']);
+        Route::put('/users/password', [AuthController::class, 'updatePassword']);
     });
 });
