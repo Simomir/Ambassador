1d70579a6ff5aca3396c069c537f08898bc57e62
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-30 13:53:46 +0300

    store method for links

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Link;
+use App\Models\LinkProduct;
+use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class LinkController extends Controller
 {
     public function index($id)
     {
         return Link::where('user_id', $id)->get();
+    }
+
+    public function store(Request $request)
+    {
+        $link = Link::create([
+            'user_id' => Auth::id(),
+            'code' => Str::random(10)
+        ]);
+
+        foreach ($request->input('products') as $product_id)
+        {
+            LinkProduct::create([
+                'link_id' => $link->id,
+                'product_id' => $product_id
+            ]);
+        }
+
+        return $link;
     }
 }
