049452edc573d02c1d05edf1fb5d66c1da0ea911
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-30 12:21:03 +0300

    removing not needed stuff

--- app/Listeners/ProductUpdatedListener.php
+++ app/Listeners/ProductUpdatedListener.php
@@ -7,22 +7,6 @@ use Illuminate\Queue\InteractsWithQueue;
 
 class ProductUpdatedListener
 {
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Handle the event.
-     *
-     * @param  object  $event
-     * @return void
-     */
     public function handle($event)
     {
         //
