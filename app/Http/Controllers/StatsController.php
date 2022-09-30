1efd4823cbd0a021475ba8de55d03f0552c003a7
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-30 09:56:03 +0300

    small refactor

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -11,14 +11,14 @@ class StatsController extends Controller
 {
     public function index()
     {
-        $links = Link::where('user_id', Auth::user()->id)->get();
+        $links = Link::where('user_id', Auth::id())->get();
         return $links->map(
             function (Link $link) {
                 $orders = Order::where('code', $link->code)->where('complete', 1)->get();
                 return [
                     'code' => $link->code,
                     'count' => $orders->count(),
-                    'revenue' => $orders->sum(fn(Order $order) => $order->ambassador_revenue),
+                    'revenue' => round($orders->sum(fn(Order $order) => $order->ambassador_revenue), 2),
                 ];
             }
         );
