f0ee95cf04710fcc1c8c44bfba76e395c8429762
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-30 14:45:47 +0300

    link relation with products

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -2,9 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\LinkFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * App\Models\Link
@@ -12,15 +17,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
  * @property int $id
  * @property string $code
  * @property int $user_id
- * @method static \Illuminate\Database\Eloquent\Builder|Link newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Link newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Link query()
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereCode($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Link whereUserId($value)
- * @mixin \Eloquent
- * @method static \Database\Factories\LinkFactory factory(...$parameters)
+ * @method static Builder|Link newModelQuery()
+ * @method static Builder|Link newQuery()
+ * @method static Builder|Link query()
+ * @method static Builder|Link whereCode($value)
+ * @method static Builder|Link whereId($value)
+ * @method static Builder|Link whereUserId($value)
+ * @mixin Eloquent
+ * @method static LinkFactory factory(...$parameters)
  * @property-read User $user
+ * @property-read Collection|Product[] $products
+ * @property-read int|null $products_count
  */
 class Link extends Model
 {
@@ -33,5 +40,10 @@ class Link extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
     }
 }
