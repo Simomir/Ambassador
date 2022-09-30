20d302bbf19c3f485f91e00c61f6729f270d3701
Simeon Shopov <simeon.t.shopov@gmail.com>
2022-09-30 12:17:34 +0300

    aded event to product controller update method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductUpdatedEvent;
 use App\Models\Product;
 use Cache;
 use Illuminate\Http\Request;
@@ -32,6 +33,7 @@ class ProductController extends Controller
     public function update(Request $request, Product $product)
     {
         $product->update($request->only('title', 'description', 'image', 'price'));
+        event(new ProductUpdatedEvent);
         return response($product, Response::HTTP_ACCEPTED);
     }
 
